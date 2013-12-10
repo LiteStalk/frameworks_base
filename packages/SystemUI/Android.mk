afb147be95c3bcf6f0740a432b62ca779eea8153
rebelos <theofficiallysonyrebel@gmail.com>
2013-12-10 22:12:25 +0200

    initial push

---
+++ packages/SystemUI/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+LOCAL_AAPT_FLAGS += --auto-add-overlay
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src) \
+    src/com/android/systemui/EventLogTags.logtags $(call all-java-files-under,../MoKeeExt/src)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res $(LOCAL_PATH)/../MoKeeExt/res
+
+LOCAL_JAVA_LIBRARIES := telephony-common
+
+LOCAL_PACKAGE_NAME := SystemUI
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
